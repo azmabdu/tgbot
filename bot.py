0f1812c6119d549557566e46d8dfc07a7436af33
Azim <user@MacBook-Abduazim.local>
2022-12-01 22:11:51 +0500

    Bug fix

--- bot.py
+++ bot.py
@@ -1,57 +1,60 @@
 import os
-import telebot
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
+from telebot import TeleBot
 from dotenv import load_dotenv
 from Publication import *
 
 load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
-bot = telebot.TeleBot(API_KEY)
+bot = TeleBot(API_KEY)
 
-button1 = telebot.types.KeyboardButton('Clothes👕')
-button2 = telebot.types.KeyboardButton('Publish📤')
+button1 = KeyboardButton('Clothes👕')
+button2 = KeyboardButton('Publish📤')
 
-markup = telebot.types.ReplyKeyboardMarkup(
+markup = ReplyKeyboardMarkup(
     row_width=1, one_time_keyboard=True, resize_keyboard=True)
 markup.add(button1, button2)
 
-jacket = telebot.types.InlineKeyboardButton('Jacket🧥', callback_data='jacket')
-t_shirt = telebot.types.InlineKeyboardButton('Shirt👕', callback_data='t_shirt')
-pants = telebot.types.InlineKeyboardButton('Pants👖', callback_data='pants')
-dress = telebot.types.InlineKeyboardButton('Dress👗', callback_data='dress')
-snickers = telebot.types.InlineKeyboardButton(
+jacket = InlineKeyboardButton('Jacket🧥', callback_data='jacket')
+t_shirt = InlineKeyboardButton('Shirt👕', callback_data='t_shirt')
+pants = InlineKeyboardButton('Pants👖', callback_data='pants')
+dress = InlineKeyboardButton('Dress👗', callback_data='dress')
+snickers = InlineKeyboardButton(
     'Snickers👟', callback_data='snickers')
-high_heel = telebot.types.InlineKeyboardButton(
+high_heel = InlineKeyboardButton(
     'High-heel👠', callback_data='high_heel')
-coat = telebot.types.InlineKeyboardButton('Coat🥼', callback_data='coat')
-skirt = telebot.types.InlineKeyboardButton('Skirt👗', callback_data='skirt')
+coat = InlineKeyboardButton('Coat🥼', callback_data='coat')
+skirt = InlineKeyboardButton('Skirt👗', callback_data='skirt')
 
 types_list = []
 
-inlineMarkup = telebot.types.InlineKeyboardMarkup(
-    row_width=2)
+inlineMarkup = InlineKeyboardMarkup(row_width=2)
 inlineMarkup.add(jacket, t_shirt, pants, dress,
                  snickers, high_heel, skirt, coat)
 types_list.extend([jacket, t_shirt, pants, dress,
                   snickers, high_heel, skirt, coat])
 
 
-new = telebot.types.InlineKeyboardButton('New', callback_data='new')
-second_hand = telebot.types.InlineKeyboardButton(
+new = InlineKeyboardButton('New', callback_data='new')
+second_hand = InlineKeyboardButton(
     'Second Hand', callback_data='second_hand')
-stateMarkup = telebot.types.InlineKeyboardMarkup(row_width=2)
+stateMarkup = InlineKeyboardMarkup(row_width=2)
 stateMarkup.add(new, second_hand)
 
 
-male = telebot.types.InlineKeyboardButton('Male', callback_data='male')
-female = telebot.types.InlineKeyboardButton('Female', callback_data='female')
-sexMarkup = telebot.types.InlineKeyboardMarkup(row_width=2)
+male = InlineKeyboardButton('Male', callback_data='male')
+female = InlineKeyboardButton('Female', callback_data='female')
+sexMarkup = InlineKeyboardMarkup(row_width=2)
 sexMarkup.add(male, female)
 
 
 @bot.message_handler(commands=['start'])
-def main(message):
-    bot.send_message(message.chat.id, 'Choose One: ', reply_markup=markup)
+def hello(message):
+    bot.send_message(message.chat.id, 'Hello, ' +
+                     message.from_user.first_name, reply_markup=markup)
+    bot.send_message(message.chat.id, 'Choose One: ',
+                     reply_markup=inlineMarkup)
 
 
 @bot.message_handler(regexp='^Clothes👕$')
@@ -67,7 +70,7 @@ def publish(message):
 
 @bot.callback_query_handler(func=lambda m: True)
 def call_query_type(call):
-    if call.data in types_list:
+    if call.data in list:
         type = call.data
     bot.edit_message_text(chat_id=call.message.chat.id,
                           message_id=call.message.message_id, text='Enter Size: ', reply_markup=None)
@@ -85,6 +88,18 @@ def get_name(message):
     name = message.text
     bot.send_message(message.chat.id, 'Choose Type: ',
                      reply_markup=inlineMarkup)
+
+
+@bot.message_handler(regex='^New$|^Second Hand$')
+def get_sex(message):
+    state = message.text
+    bot.send_message(message.chat.id, 'Choose One: ', reply_markup=sexMarkup)
+
+
+@bot.message_handler(regex='^Male$|^Female$')
+def get_photo(message):
+    sex = message.text
+    bot.send_message(message.chat.id, 'Send Photo: ')
 
 
 bot.polling()
