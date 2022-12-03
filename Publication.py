93a2a0ce325f6fcf1e9d823ba1cb8d0f6e8fd063
Azim <user@MacBook-Abduazim.local>
2022-12-03 13:29:35 +0500

    Get Image Function

--- Publication.py
+++ Publication.py
@@ -2,18 +2,11 @@ all_objects = set()
 
 
 class Product:
-    def __init__(self, name: str, type: str, size: int, sex: str, state: str, photo_url: str):
+    def __init__(self, name=None, type=None, size=None, gender=None, state=None, photo_url=None):
         all_objects.add(self)
         self.name = name
         self.type = type
         self.size = size
-        self.sex = sex
+        self.gender = gender
         self.state = state
         self.photo_url = photo_url
-
-    def __str__(self) -> str:
-        return self.name + ' ' + self.type
-
-
-p = Product('Azimjon', 'Jeans', 10, 'male', 'b/u', 'dawd')
-p2 = Product('Azizjon', 'Jacket', 10, 'male', 'b/u', 'ooo')
--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
 from telebot import TeleBot
@@ -14,7 +15,7 @@ button2 = KeyboardButton('Publish📤')
 
 markup = ReplyKeyboardMarkup(
     row_width=1, one_time_keyboard=True, resize_keyboard=True)
-markup.add(button1, button2)
+markup.add(button2, button1)
 
 jacket = InlineKeyboardButton('Jacket🧥', callback_data='jacket')
 t_shirt = InlineKeyboardButton('Shirt👕', callback_data='t_shirt')
@@ -27,13 +28,13 @@ high_heel = InlineKeyboardButton(
 coat = InlineKeyboardButton('Coat🥼', callback_data='coat')
 skirt = InlineKeyboardButton('Skirt👗', callback_data='skirt')
 
-types_list = []
+list = []
 
 inlineMarkup = InlineKeyboardMarkup(row_width=2)
 inlineMarkup.add(jacket, t_shirt, pants, dress,
                  snickers, high_heel, skirt, coat)
-types_list.extend([jacket, t_shirt, pants, dress,
-                  snickers, high_heel, skirt, coat])
+list.extend([jacket.callback_data, t_shirt.callback_data, pants.callback_data, dress.callback_data,
+             snickers.callback_data, high_heel.callback_data, skirt.callback_data, coat.callback_data])
 
 
 new = InlineKeyboardButton('New', callback_data='new')
@@ -45,61 +46,75 @@ stateMarkup.add(new, second_hand)
 
 male = InlineKeyboardButton('Male', callback_data='male')
 female = InlineKeyboardButton('Female', callback_data='female')
-sexMarkup = InlineKeyboardMarkup(row_width=2)
-sexMarkup.add(male, female)
-
-
-@bot.message_handler(commands=['start'])
-def hello(message):
-    bot.send_message(message.chat.id, 'Hello, ' +
-                     message.from_user.first_name, reply_markup=markup)
-    bot.send_message(message.chat.id, 'Choose One: ',
-                     reply_markup=inlineMarkup)
-
-
-@bot.message_handler(regexp='^Clothes👕$')
-def clothes(message):
-    for object in all_objects:
-        bot.send_message(message.chat.id, object)
+genderMarkup = InlineKeyboardMarkup(row_width=2)
+genderMarkup.add(male, female)
 
 
 @bot.message_handler(regexp='^Publish📤$')
 def publish(message):
-    bot.send_message(message.chat.id, 'Enter name: ')
+    global obj
+    obj = Product()
+    obj.name = message.from_user.first_name
+
+    bot.send_message(message.chat.id, 'Choose One 👀: ',
+                     reply_markup=inlineMarkup)
 
 
-@bot.callback_query_handler(func=lambda m: True)
+@bot.message_handler(commands=['start'])
+def hello(message):
+    name = message.from_user.first_name
+    bot.send_message(message.chat.id, 'Hello, ' +
+                     message.from_user.first_name, reply_markup=markup)
+
+
+@bot.message_handler(regexp='^Clothes👕$')
+def clothes(message):
+    print(*all_objects)
+    for object in all_objects:
+        img = object.photo_url
+        bot.send_photo(message.chat.id, photo=open(img, 'rb'),
+                       caption=f'Name 👤: {object.name}\nType 👔: {object.type.capitalize()}\nSize📎: {object.size}\nGender 🙋🏻: {object.gender}\nState ⭐️: {object.state.capitalize()}')
+
+
+@bot.callback_query_handler(func=lambda call: True)
 def call_query_type(call):
     if call.data in list:
         type = call.data
-    bot.edit_message_text(chat_id=call.message.chat.id,
-                          message_id=call.message.message_id, text='Enter Size: ', reply_markup=None)
+        obj.type = type
+        bot.send_message(chat_id=call.message.chat.id, text='Enter Size: ')
+
+    elif call.data in ['new', 'second_hand']:
+        state = call.data
+        obj.state = state
+        bot.send_message(chat_id=call.message.chat.id,
+                         text='Choose Gender: ', reply_markup=genderMarkup)
+
+    elif call.data in ['male', 'female']:
+        gender = call.data
+        obj.gender = gender
+        bot.send_message(chat_id=call.message.chat.id, text='🌄 Send Photo: ')
 
 
 @bot.message_handler(regexp='^[0-9]*$')
 def get_size(message):
     size = message.text
-    bot.send_message(message.chat.id, 'Choose State: ',
+    obj.size = size
+    bot.send_message(message.chat.id, 'Choose State 👀: ',
                      reply_markup=stateMarkup)
 
 
-@bot.message_handler(regexp='^.+$')
-def get_name(message):
-    name = message.text
-    bot.send_message(message.chat.id, 'Choose Type: ',
-                     reply_markup=inlineMarkup)
-
-
-@bot.message_handler(regex='^New$|^Second Hand$')
-def get_sex(message):
-    state = message.text
-    bot.send_message(message.chat.id, 'Choose One: ', reply_markup=sexMarkup)
-
-
-@bot.message_handler(regex='^Male$|^Female$')
+@bot.message_handler(content_types=['photo'])
 def get_photo(message):
-    sex = message.text
-    bot.send_message(message.chat.id, 'Send Photo: ')
+    fileID = message.photo[-1].file_id
+    file_info = bot.get_file(fileID)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    time = datetime.now().strftime("%H-%M-%S")
+    with open(f"recources/image-{time}.jpg", 'wb') as new_file:
+        new_file.write(downloaded_file)
+        photo_url = f"recources/image-{time}.jpg"
+        obj.photo_url = photo_url
+    bot.send_message(message.chat.id, '✅ Done!')
 
 
 bot.polling()
